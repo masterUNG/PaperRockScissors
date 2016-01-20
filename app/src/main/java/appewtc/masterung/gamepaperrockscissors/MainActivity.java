66b8f41da58c5fd66f90893efd400b6c04e9ec0d
masterUNG <phrombutr@gmail.com>
2016-01-20 10:45:32 +0700

    Bind Widget finish

--- app/src/main/java/appewtc/masterung/gamepaperrockscissors/MainActivity.java
+++ app/src/main/java/appewtc/masterung/gamepaperrockscissors/MainActivity.java
@@ -1,13 +1,35 @@
 package appewtc.masterung.gamepaperrockscissors;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ImageView paperImageView, rockImageView,
+            scissorImageView, playImageView, androidImageView;
+    private TextView showTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+        paperImageView = (ImageView) findViewById(R.id.imvPaper);
+        rockImageView = (ImageView) findViewById(R.id.imvRock);
+        scissorImageView = (ImageView) findViewById(R.id.imvScissors);
+        playImageView = (ImageView) findViewById(R.id.imvPlayer);
+        androidImageView = (ImageView) findViewById(R.id.imvAndroid);
+        showTextView = (TextView) findViewById(R.id.txtShow);
     }
-}
+
+}   // Main Class นี่คือคลาสหลัก
