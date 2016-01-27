57e9de1091a8a5a6b172c11d8170e0a633941f65
masterUNG <phrombutr@gmail.com>
2016-01-27 10:58:47 +0700

    Test Git

--- app/src/main/java/appewtc/masterung/gamepaperrockscissors/MainActivity.java
+++ app/src/main/java/appewtc/masterung/gamepaperrockscissors/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 changePlay(3);
                 myRandomPicture();
+
             }
         });
     }
